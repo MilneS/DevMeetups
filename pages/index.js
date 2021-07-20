f81f1093463034d2d63f71548dcfbf3708f8eae7
Samira Milne <milne.samira@GMAIL.COM>
2021-07-19 20:36:14 -0500

    added pages + list of meetups

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUP = [
+  {
+    id: "m1",
+    title: "A fist meetup",
+    image:
+      "https://images.unsplash.com/photo-1547482802-54e4e97a1637?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGJhcmNlbG9uYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    adress: "5 st street, 12345 City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A second meetup",
+    image:
+      "https://images.unsplash.com/photo-1547482802-54e4e97a1637?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGJhcmNlbG9uYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    adress: "10 st street, 12645 City",
+    description: "This is a second meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUP} />;
+}
+
+export default HomePage;
