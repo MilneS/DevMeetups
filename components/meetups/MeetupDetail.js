bb4af5034dbd4bafd4aab9a7337df0e1566d9d4b
Samira Milne <milne.samira@GMAIL.COM>
2021-07-20 20:39:04 -0500

    added comps and css

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import classes from "./meetupDetail.module/css";
+
+const MeetupDetail = (props) => {
+  return (
+    <section className={classes.detail}>
+      <img src={props.image} alt={props.title} />
+      <h1>{props.title}</h1>
+      <adress>{props.adress}</adress>
+      <p>{props.description}</p>
+    </section>
+  );
+};
+export default MeetupDetail;
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,16 +1,14 @@
 import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetails = () => {
   return (
-    <Fragment>
-      <img
-        src="https://images.unsplash.com/photo-1547482802-54e4e97a1637?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGJhcmNlbG9uYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
-        alt="A First Meetup"
-      />
-      <h1>A First Meetup</h1>
-      <adress>5 st street, 12345 City</adress>
-      <p>The meetup description</p>
-    </Fragment>
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1547482802-54e4e97a1637?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGJhcmNlbG9uYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+      title="A First Meetup"
+      adress="5 st street, 12345 City"
+      description="This is a first meetup"
+    />
   );
 };
 
