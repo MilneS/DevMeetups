150f0e86a970cb5c2c5637a207306eecd6fa4d3e
Samira Milne <milne.samira@GMAIL.COM>
2021-07-21 17:07:48 -0500

    added api routes

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,8 @@
+const handler=(req, res)=>{
+    if(req.method==='POST'){
+        const data= req.body;
+        const {title, image, adress, description}=data;
+    }
+}
+
+export default handler;
