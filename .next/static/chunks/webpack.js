cc6625f78fe3c5208fde4614aab6da2242b350cf
Samira Milne <milne.samira@GMAIL.COM>
2021-07-23 11:33:17 -0500

    ready for deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1cb940557c5666cebd8e";
+/******/ 	var hotCurrentHash = "40a62579a09429ad0300";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -27,7 +27,7 @@ function NewMeetupPage() {
           content="Add your own meetup and create amazing networking opportunities."
         />
       </Head>
-      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </Fragment>
   );
 }
